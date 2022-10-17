e9f15e5d16e7e7387feddbe7c9c4337b0948bfcb
Jeriel Slay <SlayJ@protonmail.com>
2022-10-17 02:18:41 -0400

    updated

--- tip-calculator/tip_calculator.py
+++ tip-calculator/tip_calculator.py
@@ -1,3 +1,4 @@
+
 print("Welcome to 'Toney's Steakhouse'")
 
 '''  
@@ -7,12 +8,13 @@ def calculate_tab():
     '''
     # Here I'm creating a try and except function so that if the user enters a value other than a float, then the original
     function will re-run
-    '''  
+    ''' 
     try: 
         bill = float(input('Enter your bill amount: $'))
-    except:
-        print('Please enter numbers only.(Decimal allowed)')
+    except ValueError:
+        print('Please enter numbers only.')
         calculate_tab()
+    
     '''
     Next, I'm doing the same thing below but using a try and except function within a while loop. So if the user inputs an
    incorrect value, it will restart just this one function, as opposed to the whole "calculate_tab" function. I've used the
@@ -22,33 +24,48 @@ def calculate_tab():
     while type(tip) != float:
         try: 
             tip = float(input('What percentage would you like to tip?: '))
-            tip2 = (bill * (tip/100))
-            total = (bill * (tip/100)) + bill
-            print(f"Your tip is: ${round(tip2, 2)}")
+            tip = (bill * (tip/100))
+            total = tip + bill
+            print(f"Your tip is: ${round(tip, 2)}")
             print(f"Bill total: ${round(total, 2)}")
         except ValueError:
-            print('Please enter numbers only.')
+            print('Please enter numbers only.(Decimal allowed)')
+        break
+            
     '''
-    Here is the same as adove...Except is catching the value error in advance.
+    Here is the same as above...Except is catching the value error in advance.
     '''
     people = False
     while type(people) != int:
         try:
             people = int(input('How many people will split the bill?: '))
+            people = total/people
         except ValueError:
             print('Please enter numbers only.')
+        
+        print(f'Each person will pay: ${round(people)}')
+        break
+
     '''
-    Next is the math that takes place to calculate all of the inputed data.
-    '''
-    bill_tip = (bill * (tip/100)) + bill
-    calculate_bill = bill_tip / people
-    '''
-    Here is where the results are being printed. I chose to use the "round" operator so that I could round the decimal to
+    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    Above is where the results are being printed. I chose to use the "round" operator so that I could round the decimal to
     the nearest 2. I did this just in case, hypothetically speaking, the user has like 200 guests on a $50 tab and tipping
     a random decimal number.
+    ===========================================================================================
+    Below, this is the progam looping itself to see if the user wants to calculate another bill.
+    vvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
     '''
-    print(f'Each person will pay: ${round(calculate_bill, 2)}')
-    print("Thank You For Dining At 'Toney's Steakhouse'")
-
-# Here is where I just called the function.
+    def quest():
+        question = str(input('Would you like to calculate another bill?: [Y]es or [N]o'))
+        while question:
+            if question.lower() == 'y':
+                calculate_tab()
+            elif question.lower() == 'n':
+                print("Thank You For Dining At 'Toney's Steakhouse'. Come Again Soon!")
+            else:
+                print('Y or N only.')
+                quest()
+            break
+        return
+    quest()
 calculate_tab()
